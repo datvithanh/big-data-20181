f116a910a3d9d6baefb3d9d94dc73b0b4f64258d
datvithanh98 <datvithanh98@gmail.com>
2018-09-13 15:07:07 +0700

    finish hw1

--- gen.py
+++ gen.py
@@ -1,23 +1,3 @@
-# lines = list(["2	0,117,135,1220,2755,12453,24539,24714,41456,45046,49927,6893,13795,16659,32828,41878", "11	0,1754,6027,7789,11142,12633,17898,19049,22486,26970,27554,27585,27591,27679,29576,32631,34906,41444"])
-
-# def gen_pair(line):
-#     pairs = []
-#     key = line.split()[0]
-#     for num in line.split()[1].split(','):
-#         pairs.append((key, num))
-#     return pairs
-
-# line = lines[0]
-# print((line.split()[0], line.split()[1].split(',')))
-
-# import sys
-# from pyspark import SparkConf, SparkContext
-
-# conf = SparkConf()
-# sc = SparkContext(conf=conf)
-# lines = sc.textFile("./hw1/data.txt")
-
-# print(lines.take(5))
 import numpy as np
 import sys
 a = np.random.random_sample([30,30]) > 0.9
--- hw1/pumn.py
+++ hw1/pumn.py
@@ -1,23 +1,50 @@
-import re
 import sys
 import os
 from pyspark import SparkConf, SparkContext
 
-def gen_pair(line):
-    pairs = []
-    key = line.split()[0]
-    if len(line.split()) > 1:
-        for num in line.split()[1].split(','):
-            pairs.append((key, num))
-    return pairs
-
 conf = SparkConf()
 sc = SparkContext(conf=conf)
-lines = sc.textFile("test.txt")
+# lines = sc.textFile("hw1/test.txt")
+lines = sc.textFile(sys.argv[1])
 
-friend_pairs = lines.flatMap(lambda l: gen_pair(l))
-mutual_friend_pairs = friend_pairs.reduceByKey(lambda n1,n2: n1.append(n2))
-res = mutual_friend_pairs
-# res.saveAsTextFile(sys.argv[2])
-# pairs = []
-# sc.stop()
+
+lines = lines.filter(lambda l: len(l.split())>1)
+friend_list = lines.map(lambda l:(l.split()[0], l.split()[1].split(',')))
+friend_pairs = friend_list.flatMapValues(lambda l: l)
+
+def mutual(pair):
+    pairs = []
+    for x in pair[1]:
+        for y in pair[1]:
+            if(x != y):
+                pairs.append((x,y))
+    return pairs
+
+def rec_list(pair):
+    rec = str(pair[0]) + "    "
+    rec_list = []
+    last_val = -1
+    for (_id, value) in pair[1]:
+        if(value == last_val):
+            rec_list.append((_id,value))
+        if(len(rec_list) >= 10):
+            continue
+        rec_list.append((_id, value))
+        if(len(rec_list) == 10):
+            last_val = value
+    first = False
+    for (_id, value) in rec_list:
+        if(first):
+            rec += ","
+        first = True
+        rec += str(_id)
+    return rec
+    
+mutual_pairs = friend_list.flatMap(mutual).subtract(friend_pairs)
+mutual_list = mutual_pairs.map(lambda w: ((int(w[0]), int(w[1])), 1)).reduceByKey(lambda n1, n2: n1 + n2)
+grouped = mutual_list.map(lambda w: (w[0][0], (w[0][1], w[1]))).sortBy(lambda w: (w[0], -w[1][1])).groupByKey().mapValues(list).sortBy(lambda w: w[0])
+res = grouped.map(rec_list)
+res.saveAsTextFile(sys.argv[2])
+sc.stop()
+# for item in res.collect():
+#     print(item)
